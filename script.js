4ef37c371d88c2393666d2e620c4960058319282
Lyuben Tenekedzhiev <lyuben.tenekedjiev@gmail.com>
2020-02-06 11:19:39 +0100

    hello again

--- script.js
+++ script.js
@@ -9,13 +9,23 @@
     newArr.push(star[i]);
     star[i].addEventListener('click', () => {
         star[i].classList.toggle('rating__star--on')
+        
+        //creating arrays
         ratingValue.textContent = newArr.indexOf(star[i]) + 1;
-        // console.log(newArr.indexOf(star[i]))
         secondArr = newArr.slice(0, newArr.indexOf(star[i]) + 1);
         thirdArr = newArr.slice(newArr.indexOf(star[i]) + 1, )
-        // console.log(secondArr)
+        
+        //adding / removing stars
         secondArr.forEach(el => el.classList.add('rating__star--on'))
         thirdArr.forEach(el => el.classList.remove('rating__star--on'));
-
     })
   }
+
+// document.querySelector('.rating__stars').addEventListener('click', function(e)  {
+//   let action = 'add';
+//   for (const span of this.children) {
+
+//       span.classList[action]('rating__star--on');
+//       if (span === e.target) action = 'remove';
+//   }
+// })
